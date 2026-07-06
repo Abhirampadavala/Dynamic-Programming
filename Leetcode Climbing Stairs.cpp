9c443c3c29054ece859aa6d53946f08ddfe03a27
Abhirampadavala <143874905+Abhirampadavala@users.noreply.github.com>
2026-07-06 11:14:38 +0530

    Update Leetcode Climbing Stairs.cpp

--- Leetcode Climbing Stairs.cpp
+++ Leetcode Climbing Stairs.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int count(int i,int n,vector<int>&dp)
+    {
+        //number of ways from i=n to n is 1
+        //number of ways from any i>n to n is 0(u cant go go backwards)
+        if(i==n) return 1;
+        if(i>n) return 0;
 
+        //if already in dp array then just return it
+        if(dp[i]!=-1) return dp[i];
+        //otherwise calculate it and store in dp vector
+        return dp[i]=count(i+1,n,dp)+count(i+2,n,dp); 
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+2,-1);
+        //here below it means that number of steps in going from 0 to nth step
+        return count(0,n,dp);
+    }
+};
