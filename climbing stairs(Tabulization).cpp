ec598a69988c899b9dcbced114cf797be1429dc1
Abhirampadavala <143874905+Abhirampadavala@users.noreply.github.com>
2026-07-16 12:41:59 +0530

    Update climbing stairs(Tabulization).cpp

--- climbing stairs(Tabulization).cpp
+++ climbing stairs(Tabulization).cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
-int climbStairs(int n) {
-  
-  }
+//Tabulization
+    int ClimbStairs(int n,vector<int>&dp)
+   {
+    if(dp[n]!=-1) return dp[n];
+    //using for loop instead of recursion
+    for(int i=3;i<=n;i++)
+    {
+    dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+   }
+
+    int climbStairs(int n) {
+        vector<int>dp(n+2,-1);
+        //base cases write first either here or in above function
+        dp[1]=1;
+        dp[2]=2;
+        return ClimbStairs(n,dp);        
+    }
 };
